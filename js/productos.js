3569410203324816559a48b27fb25529d4ac1766
Agustin-Sztrasberger <113071146+Agustin-Sztrasberger@users.noreply.github.com>
2022-11-14 01:01:35 -0300

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const productos = [];
+
+productos[0] = [];
+productos[0]['Nombre'] = 'Cerveza Noir';
+productos[0]['Precio'] = 600;
+productos[0]['Descripcion'] = 'Sabor frutado, amargo y con cuerpo denso';
+productos[0]['Imagen'] = '1.png';
+
+
+productos[1] = [];
+productos[1]['Nombre'] = 'Cerveza Noir';
+productos[1]['Precio'] = 600;
+productos[1]['Descripcion'] = 'Sabor frutado, amargo y con cuerpo denso';
+productos[1]['Imagen'] = '1.png';
+
+productos[2] = [];
+productos[2]['Nombre'] = 'Cerveza Noir';
+productos[2]['Precio'] = 600;
+productos[2]['Descripcion'] = 'Sabor frutado, amargo y con cuerpo denso';
+productos[2]['Imagen'] = '1.png';
+
