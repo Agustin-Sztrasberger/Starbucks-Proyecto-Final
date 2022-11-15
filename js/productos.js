dff0137ff492854829732bb4665b0c16a01c5003
Agustin-Sztrasberger <113071146+Agustin-Sztrasberger@users.noreply.github.com>
2022-11-15 00:37:25 -0300

    Add files via upload

--- js/productos.js
+++ js/productos.js
@@ -3,21 +3,83 @@
 const productos = [];
 
 productos[0] = [];
-productos[0]['Nombre'] = 'Cerveza Noir';
+productos[0]['Nombre'] = 'Botella verde metalizada';
 productos[0]['Precio'] = 600;
-productos[0]['Descripcion'] = 'Sabor frutado, amargo y con cuerpo denso';
-productos[0]['Imagen'] = '1.png';
+productos[0]['Descripcion'] = 'Botella Starbucks® verde metalizada.';
+productos[0]['Imagen'] = 'botella-verde.jpg';
 
 
 productos[1] = [];
-productos[1]['Nombre'] = 'Cerveza Noir';
+productos[1]['Nombre'] = 'Cuaderno espiral sirena verde';
 productos[1]['Precio'] = 600;
-productos[1]['Descripcion'] = 'Sabor frutado, amargo y con cuerpo denso';
-productos[1]['Imagen'] = '1.png';
+productos[1]['Descripcion'] = 'Cuaderno Starbucks® verde tamaño A5 con escamas de sirena y en su interior, páginas delineadas.';
+productos[1]['Imagen'] = 'cuaderno-espiral.jpg';
 
 productos[2] = [];
-productos[2]['Nombre'] = 'Cerveza Noir';
+productos[2]['Nombre'] = 'Cold Cup Terrazo Con pajita';
 productos[2]['Precio'] = 600;
-productos[2]['Descripcion'] = 'Sabor frutado, amargo y con cuerpo denso';
-productos[2]['Imagen'] = '1.png';
+productos[2]['Descripcion'] = 'Cold cup Starbucks® para bebidas frías, estilo terrazo con wordmarc y pajita.';
+productos[2]['Imagen'] = 'cup-terrazo.jpg';
+
+productos[3] = [];
+productos[3]['Nombre'] = 'Estuche Pajitas reutilizables';
+productos[3]['Precio'] = 600;
+productos[3]['Descripcion'] = 'Estuche con pajitas de diferentes colores y su limpiador, ¡un must en el merchandising Starbucks®!';
+productos[3]['Imagen'] = 'estuche-pajitas.jpg';
+
+productos[4] = [];
+productos[4]['Nombre'] = 'Llavero Cold cup detalle dorado';
+productos[4]['Precio'] = 600;
+productos[4]['Descripcion'] = 'Llavero Starbucks® con forma de vaso.';
+productos[4]['Imagen'] = 'llavero.jpg';
+
+
+productos[5] = [];
+productos[5]['Nombre'] = 'Tumbler maz verde tanslúcido';
+productos[5]['Precio'] = 600;
+productos[5]['Descripcion'] = 'Termo Starbucks® para bebidas calientes y frías transparente.';
+productos[5]['Imagen'] = 'tumbler-maz-verde.jpg';
+
+productos[6] = [];
+productos[6]['Nombre'] = 'Tumbler gris verde';
+productos[6]['Precio'] = 600;
+productos[6]['Descripcion'] = 'Termo Starbucks® para bebidas calientes y frías, verde y gris.';
+productos[6]['Imagen'] = 'tumbler-gris-verde.jpg';
+
+productos[7] = [];
+productos[7]['Nombre'] = 'Tumbler Luxor negro mate';
+productos[7]['Precio'] = 600;
+productos[7]['Descripcion'] = 'Termo Starbucks® para bebidas calientes y frías negro mate con rombos.';
+productos[7]['Imagen'] = 'tumbler-luxor-negro.jpg';
+
+productos[8] = [];
+productos[8]['Nombre'] = 'Taza Onyx negra';
+productos[8]['Precio'] = 600;
+productos[8]['Descripcion'] = 'Taza minimalista negra de Starbucks®.';
+productos[8]['Imagen'] = 'taza-onyx-negra.jpg';
+
+
+productos[9] = [];
+productos[9]['Nombre'] = 'Taza Siren Stone gris';
+productos[9]['Precio'] = 600;
+productos[9]['Descripcion'] = 'Maravillosa taza gris con estilo industrial.';
+productos[9]['Imagen'] = 'taza-siren.jpg';
+
+productos[10] = [];
+productos[10]['Nombre'] = 'Cold Cup Timi Negro mate con pajita';
+productos[10]['Precio'] = 600;
+productos[10]['Descripcion'] = 'Cold cup, para bebidas frías mate con logo de la sirena y pajita.';
+productos[10]['Imagen'] = 'timi-negro.jpg';
+
+productos[11] = [];
+productos[11]['Nombre'] = 'Circular Cup';
+productos[11]['Precio'] = 600;
+productos[11]['Descripcion'] = 'Nuestro vaso hecho con vasos reciclados.';
+productos[11]['Imagen'] = 'vaso-circular.jpg';
+
+productos[12] = [];
+productos[12]['Nombre'] = 'Vaso Reutilizable';
+productos[12]['Precio'] = 600;
+productos[12]['Descripcion'] = 'Nuestro famoso vaso reutilizable.';
+productos[12]['Imagen'] = 'vaso-reutilizable.jpg';
 
--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,11 @@ $(document).ready(function () {
     for (let i = 0; i < productos.length; i++) {
 
         $('#productos').append(
-            '<div class="col-6 col-md-4 mx-3 my-4">' +
+            '<div class="col-10 col-md-3 mx-3 my-4">' +
                 '<div class="sc-product-item">' +
-                    '<img class="img-fluid" id="fondo-producto-1" data-name="product_image" src="img/productos/'+ productos[i]['Imagen'] +'" alt="'+ productos[i]['Nombre'] +'">' +
+                '<div class="d-flex justify-content-center">' +
+                    '<img class="img-fluid mb-3" id="fondo-producto-1" data-name="product_image" src="img/productos/'+ productos[i]['Imagen'] +'" alt="'+ productos[i]['Nombre'] +'">' +
+                    '</div>' +
                     '<h4 data-name="product_name">'+ productos[i]['Nombre'] +'</h4>' +
                     '<p data-name="product_desc">'+ productos[i]['Descripcion'] +'</p>' +
                     '<input name="product_price" value="'+ productos[i]['Precio'] +'" type="hidden" >' +
@@ -22,8 +24,8 @@ $(document).ready(function () {
 
     $('#smartcart').smartCart({
 
-        cartItemTemplate: '<div class="d-flex">'+
-            //'<img class="img-fluid" src="{product_image}" />' +
+        cartItemTemplate: '<div>'+
+            '<img class="img-fluid" src="{product_image}" />' +
             '<h3 class="h5 list-group-item-heading">{product_name}</h3>' +
         '</div>',
 
